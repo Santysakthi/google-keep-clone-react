68ead59ea1fe815858e2e211deb706b363b445b7
BALA SARAVANAN.C <balasaravanan150702@gmail.com>
2022-11-02 22:34:43 +0530

    Add files via upload

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import HighlightIcon from '@mui/icons-material/Highlight';
+import "./Header.css";
+const Header = () => {
+  return (
+    <div>
+        <header>
+            <h1>  
+                <HighlightIcon />
+                Keep
+            </h1>
+        </header>
+    </div>
+    
+  )
+}
+
+export default Header
